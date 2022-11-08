38da913d93382cc133cf264332b7e5a2f52debc6
huangjianqin <459564034@qq.com>
2022-11-08 17:35:09 +0800

    mqtt server优化

--- kin-mqtt-broker-starter/src/main/java/org/kin/mqtt/broker/MqttBrokerAutoConfiguration.java
+++ kin-mqtt-broker-starter/src/main/java/org/kin/mqtt/broker/MqttBrokerAutoConfiguration.java
@@ -5,6 +5,10 @@ import io.netty.buffer.PooledByteBufAllocator;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
+import io.netty.channel.ServerChannel;
+import io.netty.channel.epoll.Epoll;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -34,11 +38,11 @@ public class MqttBrokerAutoConfiguration {
      */
     @Bean
     public DisposableBean mqttServerDisposable() throws Exception {
-        EventLoopGroup boss = new NioEventLoopGroup();
-        EventLoopGroup worker = new NioEventLoopGroup();
+        EventLoopGroup boss = getAdaptiveEventLoopGroup(2);
+        EventLoopGroup worker = getAdaptiveEventLoopGroup(0);
         ServerBootstrap serverBootstrap = new ServerBootstrap();
         serverBootstrap.group(boss, worker)
-                .channel(NioServerSocketChannel.class)
+                .channel(getAdaptiveServerChannelClass())
                 .childOption(ChannelOption.TCP_NODELAY, true)
                 .childOption(ChannelOption.SO_REUSEADDR, true)
                 .childOption(ChannelOption.SO_KEEPALIVE, true)
@@ -56,5 +60,26 @@ public class MqttBrokerAutoConfiguration {
             boss.shutdownGracefully();
             worker.shutdownGracefully();
         };
+    }
+
+    /**
+     * 根据部署环境获取合适的{@link EventLoopGroup}实现, nio or epoll
+     */
+    private static EventLoopGroup getAdaptiveEventLoopGroup(int nThreads) {
+        nThreads = Math.max(nThreads, 0);
+        if (Epoll.isAvailable()) {
+            return new EpollEventLoopGroup(nThreads);
+        }
+        return new NioEventLoopGroup(nThreads);
+    }
+
+    /**
+     * 根据部署环境获取合适的{@link ServerChannel}实现, nio or epoll
+     */
+    private static Class<? extends ServerChannel> getAdaptiveServerChannelClass() {
+        if (Epoll.isAvailable()) {
+            return EpollServerSocketChannel.class;
+        }
+        return NioServerSocketChannel.class;
     }
 }
