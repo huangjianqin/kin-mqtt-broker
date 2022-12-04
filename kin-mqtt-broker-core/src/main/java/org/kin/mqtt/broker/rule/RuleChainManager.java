73749b73ee66dadb167c119a3cc11c260f762e91
huangjianqin <459564034@qq.com>
2022-12-04 10:06:52 +0800

    新增更新和移除规则链接口

--- kin-mqtt-broker-core/src/main/java/org/kin/mqtt/broker/rule/RuleChainManager.java
+++ kin-mqtt-broker-core/src/main/java/org/kin/mqtt/broker/rule/RuleChainManager.java
@@ -7,10 +7,7 @@ import org.kin.mqtt.broker.rule.impl.ScriptRule;
 import org.kin.mqtt.broker.rule.impl.TopicRule;
 
 import javax.annotation.Nullable;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * @author huangjianqin
@@ -37,6 +34,16 @@ public final class RuleChainManager {
      * @param definition 规则链定义
      */
     public void addRuleChain(RuleChainDefinition definition) {
+        updateRuleChain0(definition, false);
+    }
+
+    /**
+     * 更新规则链
+     *
+     * @param definition 规则链定义
+     * @param overwrite  是否允许覆盖
+     */
+    private void updateRuleChain0(RuleChainDefinition definition, boolean overwrite) {
         List<RuleDefinition> childDefinitions = definition.getChain();
         int size = childDefinitions.size();
         RuleNode nextNode = parseDefinition(childDefinitions.get(size - 1), null);
@@ -46,30 +53,10 @@ public final class RuleChainManager {
             nextNode = curNode;
         }
         String name = definition.getName();
-        if (Objects.nonNull(
-                ruleChains.put(name, new RuleChain(name, definition.getDesc(), curNode))
-        )) {
+        if (Objects.nonNull(ruleChains.put(name, new RuleChain(name, definition.getDesc(), curNode))) &&
+                !overwrite) {
             throw new IllegalStateException(String.format("rule chain name '%s' conflict!!", name));
         }
-        ;
-    }
-
-    /**
-     * 根据名字匹配规则链
-     *
-     * @param name 规则链名字
-     * @return {@link RuleChain}
-     */
-    @Nullable
-    public RuleChain getRuleChain(String name) {
-        return ruleChains.get(name);
-    }
-
-    /**
-     * @return 所有规则链
-     */
-    public Collection<RuleChain> getRuleChains() {
-        return ruleChains.values();
     }
 
     /**
@@ -94,5 +81,63 @@ public final class RuleChainManager {
             default:
                 throw new IllegalArgumentException("unable to parse rule definition " + definition);
         }
+    }
+
+    /**
+     * 更新规则链
+     *
+     * @param definition 规则链定义
+     */
+    public void updateRuleChain(RuleChainDefinition definition) {
+        updateRuleChain0(definition, true);
+    }
+
+    /**
+     * 批量更新规则链
+     *
+     * @param definitions 规则链定义list
+     */
+    public void updateRuleChains(List<RuleChainDefinition> definitions) {
+        for (RuleChainDefinition definition : definitions) {
+            updateRuleChain(definition);
+        }
+    }
+
+    /**
+     * 批量移除规则链
+     *
+     * @param chainNames 规则链唯一命名集合
+     */
+    public void removeRuleChains(Collection<String> chainNames) {
+        for (String chainName : chainNames) {
+            ruleChains.remove(chainName);
+        }
+    }
+
+    /**
+     * 批量移除规则链
+     *
+     * @param chainNames 规则链唯一命名集合
+     */
+    public void removeRuleChains(String... chainNames) {
+        removeRuleChains(Arrays.asList(chainNames));
+    }
+
+    /**
+     * 根据名字匹配规则链
+     *
+     * @param name 规则链名字
+     * @return {@link RuleChain}
+     */
+    @Nullable
+    public RuleChain getRuleChain(String name) {
+        return ruleChains.get(name);
+    }
+
+    /**
+     * @return 所有规则链
+     */
+    public Collection<RuleChain> getRuleChains() {
+        return ruleChains.values();
     }
 }
