7daf6fc4b9fd4e95340f0cb962c4f3993abd72dd
huangjianqin <459564034@qq.com>
2023-01-02 17:29:05 +0800

    描述

--- kin-mqtt-broker-auth/kin-mqtt-broker-auth-db-starter/src/main/db/schema.sql
+++ kin-mqtt-broker-auth/kin-mqtt-broker-auth-db-starter/src/main/db/schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `kin_mqtt_user`
 (
-    `id`          int(11) unsigned NOT NULL AUTO_INCREMENT,
-    `username`    varchar(100) DEFAULT NULL,
-    `password`    varchar(100) DEFAULT NULL,
-    `salt`        varchar(35)  DEFAULT NULL,
-    `create_time` datetime     DEFAULT NULL,
+    `id`          int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+    `username`    varchar(100) DEFAULT NULL COMMENT '用户名',
+    `password`    varchar(100) DEFAULT NULL COMMENT '密码',
+    `salt`        varchar(35)  DEFAULT NULL COMMENT '加盐',
+    `create_time` datetime     DEFAULT NULL COMMENT '创建时间',
     PRIMARY KEY (`id`),
     UNIQUE KEY `kin_mqtt_username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
